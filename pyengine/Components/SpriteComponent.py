b2bdd2178010caff0d5114729e6bd6e760801835
LavaPower <lavapower84@gmail.com>
2019-04-12 23:37:36 +0200

    Components : Update SpriteComponent

--- pyengine/Components/SpriteComponent.py
+++ pyengine/Components/SpriteComponent.py
@@ -1,17 +1,28 @@
 import pygame
 from pyengine.Exceptions import ComponentAlreadyIntialized
+from pyengine.Components.PositionComponent import PositionComponent
 
 __all__ = ["SpriteComponent"]
 
 
 class SpriteComponent:
+    name = "SpriteComponent"
+
     def __init__(self):
-        self.image = None
         self.entity = None
         self.initialized = False
 
-    def initialize(self, image, entity):
+    def initialize(self, entity, image):
         if self.initialized:
             raise ComponentAlreadyIntialized("SpriteComponent already initialized")
-        self.image = image
         self.entity = entity
+        self.entity.sprite = image
+        self.entity.image = pygame.image.load(image)
+        if self.entity.has_component(PositionComponent):
+            self.entity.rect = self.entity.image.get_rect()
+            self.entity.rect.x = self.entity.x
+            self.entity.rect.y = self.entity.y
+
+    def update_position(self):
+        self.entity.rect.x = self.entity.x
+        self.entity.rect.y = self.entity.y
