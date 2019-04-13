44c3ce820954ee8b0ba80ebe2978eb3c6d3624ef
LavaPower <lavapower84@gmail.com>
2019-04-13 22:21:58 +0200

    Tests : Test PhysicsComponent's Callback

--- tests/test.py
+++ tests/test.py
@@ -2,6 +2,10 @@ from pyengine import Window, World, Entity, ControlType
 from pyengine.Components import *
 
 
+def collision(id1, id2):
+    print("Collision between", id1, "and", id2)
+
+
 game = Window(800, 600, True)
 monde = World()
 game.set_world(monde)
@@ -10,7 +14,8 @@ entity = Entity()
 entity.add_components(PositionComponent, [100, 100])
 entity.add_components(SpriteComponent, "images/sprite0.png")
 entity.add_components(ControlComponent, ControlType.CLASSICJUMP, 5)
-entity.add_components(PhysicsComponent)
+phys = entity.add_components(PhysicsComponent)
+phys.set_callback(collision)
 
 subentity = Entity()
 subentity.add_components(PositionComponent, [100, 100], [10, -10])
