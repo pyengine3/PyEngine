65b056e5928e8db38617baeddfaacefd7ff2bc16
LavaPower <lavapower84@gmail.com>
2019-04-13 13:08:19 +0200

    Tests : Update

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,5 @@
-from pyengine import Window, World, Entity
-from pyengine.Components import PositionComponent, SpriteComponent
+from pyengine import Window, World, Entity, ControlType
+from pyengine.Components import *
 
 
 game = Window(800, 600)
@@ -9,6 +9,8 @@ game.set_world(monde)
 entity = Entity()
 entity.add_components(PositionComponent, [100, 100])
 entity.add_components(SpriteComponent, "images/sprite0.png")
+entity.add_components(ControlComponent, ControlType.FOURDIRECTION, 2)
+entity.add_components(PhysicsComponent, True, 2)
 
 subentity = Entity()
 subentity.add_components(PositionComponent, [100, 100], [10, 10])
@@ -16,8 +18,12 @@ subentity.add_components(SpriteComponent, "images/sprite0.png")
 
 entity.attach_entity(subentity)
 
-entity.get_component(PositionComponent).set_position([200, 200])
+bloc = Entity()
+bloc.add_components(PositionComponent, [300, 300])
+bloc.add_components(SpriteComponent, "images/sprite1.png")
+bloc.add_components(PhysicsComponent, True, 5)
 
+monde.add_entity(bloc)
 monde.add_entity(entity)
 monde.add_entity(subentity)
 
