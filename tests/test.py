07b4b1ac7ca0f5ff5b88eae3da122af76932677b
LavaPower <lavapower84@gmail.com>
2019-04-25 21:59:40 +0200

    Test : Test CollisionCallback / WorldCallback / LifeBarComponent

--- tests/test.py
+++ tests/test.py
@@ -1,29 +1,32 @@
 from pyengine import Window, World, Entity, ControlType
 from pyengine.Components import *
 from pyengine.Systems import *
+from pyengine.Enums import WorldCallbacks
+
+
+def collision(objet, cause):
+    print("COLLISION")
+
+
+def fall(objet):
+    if objet.has_component(PositionComponent):
+        position = objet.get_component(PositionComponent)
+        position.set_position([position.x, 0])
+    print("FALL :", objet.id)
 
 
 game = Window(800, 600, True)
 monde = World()
+monde.set_callback(WorldCallbacks.FALL, fall)
 game.set_world(monde)
-
-
-def collision(objet):
-    monde.get_system(MusicSystem).next_song()
-
 
 entity = Entity()
 entity.add_components(PositionComponent, [100, 100])
 entity.add_components(SpriteComponent, "images/sprite0.png")
 entity.add_components(ControlComponent, ControlType.DOUBLEJUMP)
+entity.add_components(LifeBarComponent, 100, ["images/lifebar-back.png", "images/lifebar-front.png"], [-30, -15])
 phys = entity.add_components(PhysicsComponent)
 phys.set_callback(collision)
-
-subentity = Entity()
-subentity.add_components(PositionComponent, [100, 100], [10, -10])
-subentity.add_components(TextComponent, "TEST")
-
-entity.attach_entity(subentity)
 
 bloc = Entity()
 bloc.add_components(PositionComponent, [300, 300])
@@ -38,14 +41,11 @@ entitySystem = monde.get_system(EntitySystem)
 entitySystem.add_entity(bloc)
 entitySystem.add_entity(bloc2)
 entitySystem.add_entity(entity)
-entitySystem.add_entity(subentity)
+
+lifebar = entity.get_component(LifeBarComponent)
+lifebar.create_life_sprites()
+lifebar.update_life(50)
 
 bloc.get_component(SpriteComponent).set_rotation(45)
-
-musicSystem = monde.get_system(MusicSystem)
-musicSystem.set_loop(True)
-musicSystem.add("musique/Heikousen.mp3")
-musicSystem.add("musique/Richard.mp3")
-musicSystem.play()
 
 game.run()
