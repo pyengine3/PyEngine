aabd7ddfbbd89abf3ec078d6873878b11000ffe2
LavaPower <lavapower84@gmail.com>
2019-04-25 12:42:14 +0200

    PyEngine : Change version

--- pyengine/__init__.py
+++ pyengine/__init__.py
@@ -5,4 +5,4 @@ from pyengine.Enums import ControlType, MouseButton
 from pygame import locals as const
 
 __all__ = ["Window", "World", "Entity", "ControlType", "const", "MouseButton"]
-__version__="0.1.0-DEV"
+__version__="0.2.0-DEV"
