ac3d3006c7e99560e80fa788916b83908a03c11c
LavaPower <lavapower84@gmail.com>
2019-04-28 20:40:24 +0200

    test : Test modify widget

--- tests/test.py
+++ tests/test.py
@@ -6,7 +6,10 @@ from random import randint
 
 
 def click(obj, b):
-    obj.set_position([randint(0, 800-100), randint(0, 600-100)])
+    label.set_color((randint(0, 255), randint(0, 255), randint(0, 255)))
+    obj.get_label().set_color((randint(0, 255), randint(0, 255), randint(0, 255)))
+    obj.get_label().set_text("CECI EST BIEN TROP LONG !")
+    obj.update()
 
 
 game = Window(800, 600, True)
@@ -16,11 +19,11 @@ game.add_state(state)
 
 monde = state.get_world()
 
-button = Button([100, 100], "Appuie", click, [100, 100])
-button2 = Button([150, 150], "Appuie 2", click, [100, 100], "images/sprite0.png")
+button = Button([100, 100], "Appuie", click)
+label = Label([200, 200], "RAINBOW !", None, ["arial", 35])
 
 system = monde.get_system(UISystem)
 system.add_widget(button)
-system.add_widget(button2)
+system.add_widget(label)
 
 game.run()
