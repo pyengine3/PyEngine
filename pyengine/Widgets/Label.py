e3f5240999b33723aec5551725b473c7f7398f57
LavaPower <lavapower84@gmail.com>
2019-04-28 20:39:52 +0200

    Label : Create functions to modify Label

--- pyengine/Widgets/Label.py
+++ pyengine/Widgets/Label.py
@@ -20,7 +20,40 @@ class Label(Widget):
             font.append(False)
 
         self.color = color
+        self.font_infos = font
         self.font = pygame.font.SysFont(font[0], font[1], font[2], font[3])
         self.text = text
         self.image = self.font.render(text, 1, color)
         self.update_rect()
+
+    def set_color(self, color):
+        self.color = color
+        self.image = self.font.render(self.text, 1, color)
+        self.update_rect()
+
+    def get_color(self):
+        return self.color
+
+    def set_font(self, font):
+        if len(font) == 1:
+            font.append(15)
+        if len(font) == 2:
+            font.append(False)
+        if len(font) == 3:
+            font.append(False)
+        self.font_infos = font
+        self.font = pygame.font.SysFont(font[0], font[1], font[2], font[3])
+        self.image = self.font.render(self.text, 1, self.color)
+        self.update_rect()
+
+    def get_font(self):
+        return self.font_infos
+
+    def get_text(self):
+        return self.text
+
+    def set_text(self, text):
+        self.text = text
+        self.image = self.font.render(text, 1, self.color)
+        self.update_rect()
+
