7e120dd70da9e7bcd2a14e95a7ee0c94db0a8ac9
LavaPower <lavapower84@gmail.com>
2019-04-29 14:09:13 +0200

    Image : Add getters and setters for size and image

--- pyengine/Widgets/Image.py
+++ pyengine/Widgets/Image.py
@@ -8,7 +8,25 @@ class Image(Widget):
     def __init__(self, position, image, size=None):
         super(Image, self).__init__(position)
 
+        self.istr = image
         self.image = pygame.image.load(image)
         if size is not None:
             self.image = pygame.transform.scale(self.image, size)
+        self.update_rect()
+
+    def get_image(self):
+        return self.istr
+
+    def set_image(self, image, size=None):
+        self.istr = image
+        self.image = pygame.image.load(image)
+        if size is not None:
+            self.image = pygame.transform.scale(self.image, size)
+        self.update_rect()
+
+    def get_size(self):
+        return [self.rect.width, self.rect.height]
+
+    def set_size(self, size):
+        self.image = pygame.transform.scale(self.image, size)
         self.update_rect()
