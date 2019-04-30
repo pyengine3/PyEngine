bcd28bc624649512e9f5dc7691f2da0963fa2a6d
LavaPower <lavapower84@gmail.com>
2019-04-30 21:35:52 +0200

    Entry : Create

---
+++ pyengine/Widgets/Entry.py
@@ -0,0 +1,69 @@
+import pygame
+from pyengine.Widgets.Widget import Widget
+from pyengine.Widgets import Label
+from pygame import locals as const
+
+__all__ = ["Entry"]
+
+
+class Entry(Widget):
+    def __init__(self, position, width=200):
+        super(Entry, self).__init__(position)
+
+        self.width = width
+
+        self.image = pygame.Surface([self.width, 35])
+        self.image.fill((50, 50, 50))
+        self.iiwhite = pygame.Surface([self.width-8, 28])
+        self.iiwhite.fill((255, 255, 255))
+        self.image.blit(self.iiwhite, (4, 4))
+        self.label = Label([position[0]+5, position[1]+5], "Test", (0, 0, 0), ["arial", 17])
+        self.label.parent = self
+        self.cursortimer = 20
+        self.cursor = False
+        self.typing = False
+        self.update_all()
+
+    def focusout(self):
+        if self.cursor:
+            self.label.set_text(self.label.get_text()[:-1])
+        self.cursor = False
+        self.typing = False
+
+    def keyup(self, evt):
+        self.typing = False
+
+    def keypress(self, evt):
+        if evt.key == const.K_BACKSPACE and not self.typing:
+            if len(self.label.get_text()):
+                if self.cursor:
+                    self.label.set_text(self.label.get_text()[:-2]+"I")
+                else:
+                    self.label.set_text(self.label.get_text()[:-1])
+        elif evt.unicode and not self.typing:
+            if self.cursor:
+                self.label.set_text(self.label.get_text()[:-1]+evt.unicode+"I")
+            else:
+                self.label.set_text(self.label.get_text() + evt.unicode)
+        self.typing = True
+
+    def update_all(self):
+        self.update_rect()
+        self.label.set_position([self.label.get_position()[0],
+                                 self.rect.y + 4 + self.iiwhite.get_rect().height / 2
+                                 - self.label.rect.height / 2])
+
+    def set_system(self, system):
+        super(Entry, self).set_system(system)
+        system.add_widget(self.label)
+
+    def update(self):
+        if self.cursortimer <= 0:
+            if self.cursor:
+                self.label.set_text(self.label.get_text()[:-1])
+            else:
+                self.label.set_text(self.label.get_text()+"I")
+            self.cursor = not self.cursor
+            self.cursortimer = 20
+        self.cursortimer -= 1
+
--- pyengine/Widgets/__init__.py
+++ pyengine/Widgets/__init__.py
@@ -1,5 +1,6 @@
 from pyengine.Widgets.Label import Label
 from pyengine.Widgets.Image import Image
 from pyengine.Widgets.Button import Button
+from pyengine.Widgets.Entry import Entry
 
-__all__ = ["Label", "Image", "Button"]
+__all__ = ["Label", "Image", "Button", "Entry"]
