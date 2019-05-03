0c80576b2d2539e5d903bc13eb5cf97cc92ff1e5
LavaPower <lavapower84@gmail.com>
2019-05-03 13:16:25 +0200

    Test : Test MoveComponent

--- tests/test.py
+++ tests/test.py
@@ -1,33 +1,40 @@
-from pyengine import Window, GameState
+from pyengine import Window, GameState, Entity
 from pyengine.Systems import *
-from pyengine.Widgets import *
-
-from random import randint
+from pyengine.Components import *
 
 
-def click(obj, b):
-    if image.get_image() == "images/sprite0.png":
-        image.set_image("images/sprite1.png")
-    else:
-        image.set_image("images/sprite0.png")
-    obj.set_size([randint(100, 200), randint(100, 200)])
-    print(entry.get_text())
+def collision(obj, cause):
+    direction = ball.get_component(MoveComponent).get_direction()
+    ball.get_component(MoveComponent).set_direction([0, -direction[1]])
 
 
 game = Window(800, 600, (200, 145, 208), True)
-game.set_title("UI TEST")
+game.set_title("Move TEST")
 state = GameState("BASE")
 game.add_state(state)
 
 monde = state.get_world()
 
-button = Button([100, 100], "Appuie", click)
-image = Image([200, 200], "images/sprite0.png")
-entry = Entry([300, 300])
+bloc1 = Entity()
+bloc1.add_component(PositionComponent([100, 100]))
+bloc1.add_component(SpriteComponent("images/sprite0.png"))
+bloc1.add_component(PhysicsComponent(False))
 
-system = monde.get_system(UISystem)
-system.add_widget(button)
-system.add_widget(image)
-system.add_widget(entry)
+bloc2 = Entity()
+bloc2.add_component(PositionComponent([100, 300]))
+bloc2.add_component(SpriteComponent("images/sprite0.png"))
+bloc2.add_component(PhysicsComponent(False))
+
+ball = Entity()
+ball.add_component(PositionComponent([100, 200]))
+ball.add_component(SpriteComponent("images/sprite1.png"))
+physics = ball.add_component(PhysicsComponent(False))
+physics.set_callback(collision)
+ball.add_component(MoveComponent([0, 1]))
+
+system = monde.get_system(EntitySystem)
+system.add_entity(bloc1)
+system.add_entity(bloc2)
+system.add_entity(ball)
 
 game.run()
