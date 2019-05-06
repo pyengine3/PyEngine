96e9594d4a6fe24cdba3ee093dc1232b83dd4b2a
LavaPower <lavapower84@gmail.com>
2019-05-06 16:34:13 +0200

    Test : Retest Entry

--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,6 @@
-from pyengine import Window, GameState, Entity
+from pyengine import Window, GameState
 from pyengine.Systems import *
-from pyengine.Components import *
-
-
-def collision(obj, cause):
-    direction = ball.get_component(MoveComponent).get_direction()
-    ball.get_component(MoveComponent).set_direction([0, -direction[1]])
+from pyengine.Widgets import *
 
 
 game = Window(800, 600, (200, 145, 208), True)
@@ -15,26 +10,9 @@ game.add_state(state)
 
 monde = state.get_world()
 
-bloc1 = Entity()
-bloc1.add_component(PositionComponent([100, 100]))
-bloc1.add_component(SpriteComponent("images/sprite0.png"))
-bloc1.add_component(PhysicsComponent(False))
+entry = Entry([100, 100])
 
-bloc2 = Entity()
-bloc2.add_component(PositionComponent([100, 300]))
-bloc2.add_component(SpriteComponent("images/sprite0.png"))
-bloc2.add_component(PhysicsComponent(False))
-
-ball = Entity()
-ball.add_component(PositionComponent([100, 200]))
-ball.add_component(SpriteComponent("images/sprite1.png"))
-physics = ball.add_component(PhysicsComponent(False))
-physics.set_callback(collision)
-ball.add_component(MoveComponent([0, 1]))
-
-system = monde.get_system(EntitySystem)
-system.add_entity(bloc1)
-system.add_entity(bloc2)
-system.add_entity(ball)
+system = monde.get_system(UISystem)
+system.add_widget(entry)
 
 game.run()
