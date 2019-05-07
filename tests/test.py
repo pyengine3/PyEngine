61ccb842bb6239b77ca2c91503785f3bcc17f722
LavaPower <lavapower84@gmail.com>
2019-05-07 15:00:07 +0200

    Test : Test controls

--- tests/test.py
+++ tests/test.py
@@ -1,18 +1,32 @@
-from pyengine import Window, GameState
+from pyengine import Window, GameState, Entity
 from pyengine.Systems import *
-from pyengine.Widgets import *
+from pyengine.Components import *
+from pyengine.Enums import ControlType, Controls
+from pyengine.Components.ControlComponent import const
 
 
 game = Window(800, 600, (200, 145, 208), True)
-game.set_title("Move TEST")
+game.set_title("Controles TEST")
 state = GameState("BASE")
 game.add_state(state)
 
 monde = state.get_world()
 
-entry = Entry([100, 100])
+entity = Entity()
+entity.add_component(PositionComponent([100, 100]))
+entity.add_component(SpriteComponent("images/sprite0.png"))
+entity.add_component(PhysicsComponent())
+control = entity.add_component(ControlComponent(ControlType.CLASSICJUMP))
+control.set_control(Controls.UPJUMP, const.K_SPACE)
 
-system = monde.get_system(UISystem)
-system.add_widget(entry)
+bloc = Entity()
+bloc.add_component(PositionComponent([100, 200]))
+bloc.add_component(SpriteComponent("images/sprite1.png"))
+bloc.add_component(PhysicsComponent(False))
+
+
+system = monde.get_system(EntitySystem)
+system.add_entity(entity)
+system.add_entity(bloc)
 
 game.run()
