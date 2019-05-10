5f247c55042864eb91b33d75ca851d2b771e1bd0
LavaPower <lavapower84@gmail.com>
2019-05-10 17:47:00 +0200

    __init__ : Fix ModuleNotFoundError on install of PyEngine

--- pyengine/__init__.py
+++ pyengine/__init__.py
@@ -1,9 +1,12 @@
-from pyengine.Window import Window
-from pyengine.World import World
-from pyengine.Entity import Entity
-from pyengine.GameState import GameState
-from pyengine.Enums import ControlType, MouseButton, CollisionCauses, WorldCallbacks
-from pygame import locals as const
+try:
+    from pyengine.Window import Window
+    from pyengine.World import World
+    from pyengine.Entity import Entity
+    from pyengine.GameState import GameState
+    from pyengine.Enums import ControlType, MouseButton, CollisionCauses, WorldCallbacks
+    from pygame import locals as const
+except ModuleNotFoundError:
+    pass
 
 __all__ = ["Window", "World", "Entity", "ControlType", "const", "MouseButton", "CollisionCauses",
            "WorldCallbacks", "GameState"]
