96e79ddb7617a166949dc9b095a194a4fa8f70d0
LavaPower <lavapower84@gmail.com>
2019-05-18 11:08:34 +0200

    Components : Rename LifeCOmponent in __init__

--- pyengine/Components/__init__.py
+++ pyengine/Components/__init__.py
@@ -3,8 +3,8 @@ from pyengine.Components.PositionComponent import PositionComponent
 from pyengine.Components.ControlComponent import ControlComponent
 from pyengine.Components.PhysicsComponent import PhysicsComponent
 from pyengine.Components.TextComponent import TextComponent
-from pyengine.Components.LifeBarComponent import LifeBarComponent
+from pyengine.Components.LifeComponent import LifeComponent
 from pyengine.Components.MoveComponent import MoveComponent
 
 __all__ = ["SpriteComponent", "PositionComponent", "ControlComponent", "PhysicsComponent",
-           "TextComponent", "LifeBarComponent", "MoveComponent"]
+           "TextComponent", "LifeComponent", "MoveComponent"]
