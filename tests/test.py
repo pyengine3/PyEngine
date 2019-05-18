68f15943997e717b9a466fa905556a41c986594f
LavaPower <lavapower84@gmail.com>
2019-05-18 11:11:21 +0200

    Test : Update

--- tests/test.py
+++ tests/test.py
@@ -1,16 +1,18 @@
-from pyengine import Window, GameState, Entity
+from pyengine import Window, GameState, Entity, ControlType, Controls, StateCallbacks
 from pyengine.Systems import *
 from pyengine.Components import *
-from pyengine.Enums import ControlType, Controls
 from pyengine.Components.ControlComponent import const
+
+
+def quitter(obj, pos):
+    if state.get_system(EntitySystem).has_entity(bloc):
+        state.get_system(EntitySystem).remove_entity(bloc)
 
 
 game = Window(800, 600, (200, 145, 208), True)
 game.set_title("Controles TEST")
 state = GameState("BASE")
 game.add_state(state)
-
-monde = state.get_world()
 
 entity = Entity()
 entity.add_component(PositionComponent([100, 100]))
@@ -19,13 +21,15 @@ entity.add_component(PhysicsComponent())
 control = entity.add_component(ControlComponent(ControlType.CLASSICJUMP))
 control.set_control(Controls.UPJUMP, const.K_SPACE)
 
+state.set_callback(StateCallbacks.OUTOFWINDOW, quitter)
+
 bloc = Entity()
 bloc.add_component(PositionComponent([100, 200]))
-bloc.add_component(SpriteComponent("images/sprite1.png"))
+bloc.add_component(SpriteComponent("images/sprite1.png", 1, 45))
 bloc.add_component(PhysicsComponent(False))
 
 
-system = monde.get_system(EntitySystem)
+system = state.get_system(EntitySystem)
 system.add_entity(entity)
 system.add_entity(bloc)
 
