6d1355f834cbf7aae9fd3e19e61827be9ec152f2
LavaPower <lavapower84@gmail.com>
2019-05-18 11:10:38 +0200

    __init__ : Apply changes and change version

--- pyengine/__init__.py
+++ pyengine/__init__.py
@@ -1,13 +1,12 @@
 try:
     from pyengine.Window import Window
-    from pyengine.World import World
     from pyengine.Entity import Entity
     from pyengine.GameState import GameState
-    from pyengine.Enums import ControlType, MouseButton, CollisionCauses, WorldCallbacks
+    from pyengine.Enums import ControlType, MouseButton, CollisionCauses, StateCallbacks, Controls
     from pygame import locals as const
 except ModuleNotFoundError:
     pass
 
-__all__ = ["Window", "World", "Entity", "ControlType", "const", "MouseButton", "CollisionCauses",
-           "WorldCallbacks", "GameState"]
-__version__ = "1.0.2"
+__all__ = ["Window", "Entity", "ControlType", "const", "MouseButton", "CollisionCauses",
+           "StateCallbacks", "GameState", "Controls"]
+__version__ = "1.1.0"
