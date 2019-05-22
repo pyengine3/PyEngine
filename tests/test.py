db6091c7bc07e663a2c1fc26ef47f538db9a06f9
LavaPower <lavapower84@gmail.com>
2019-05-22 13:44:17 +0200

    Test : Test entry background

--- tests/test.py
+++ tests/test.py
@@ -3,25 +3,15 @@ from pyengine.Systems import *
 from pyengine.Widgets import *
 
 
-def click(obj, button):
-    if image.is_show():
-        image.hide()
-    else:
-        image.show()
-
-
 game = Window(800, 600, (200, 145, 208), True)
 game.set_title("ShowHide TEST")
 state = GameState("BASE")
 game.add_state(state)
 
-button = Button([100, 100], "Clique", click)
-
-image = Image([300, 300], "images/sprite0.png")
+entry = Entry([100, 100], 200, "images/sprite0.png")
 
 
 system = state.get_system(UISystem)
-system.add_widget(button)
-system.add_widget(image)
+system.add_widget(entry)
 
 game.run()
