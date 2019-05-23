c157cce7c49d01f06e71dc1576add0537a7dc1da
LavaPower <lavapower84@gmail.com>
2019-05-23 20:43:56 +0200

    TextComponent : Use Font object

--- pyengine/Components/TextComponent.py
+++ pyengine/Components/TextComponent.py
@@ -1,5 +1,5 @@
 from pyengine.Exceptions import CompatibilityError
-import pygame
+from pyengine.Utils import Font
 
 __all__ = ["TextComponent"]
 
@@ -7,20 +7,16 @@ __all__ = ["TextComponent"]
 class TextComponent:
     def __init__(self, texte, color=None, font=None):
         if font is None:
-            font = ["arial", 12, False, False]
+            font = Font()
         if color is None:
             color = (255, 255, 255)
 
-        if len(font) == 1:
-            font.append(12)
-        if len(font) == 2:
-            font.append(False)
-        if len(font) == 3:
-            font.append(False)
+        if not isinstance(font, Font):
+            raise TypeError("Font have not a Font type")
 
         self.entity = None
         self.texte = texte
-        self.font = pygame.font.SysFont(font[0], font[1], font[2], font[3])
+        self.font = font
         self.color = color
 
     def set_entity(self, entity):
@@ -32,5 +28,20 @@ class TextComponent:
 
         self.entity = entity
 
+    def set_color(self, color):
+        self.color = color
+
+    def get_color(self):
+        return self.color
+
+    def set_font(self, font):
+        if not isinstance(font, Font):
+            raise TypeError("Font have not a Font type")
+
+        self.font = font
+
+    def get_font(self):
+        return self.font
+
     def render(self):
-        return self.font.render(self.texte, 1, self.color)
+        return self.font.render().render(self.texte, 1, self.color)
