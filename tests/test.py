dc74556867a59ffd62a229c83a307fc7510377fc
LavaPower <lavapower84@gmail.com>
2019-05-23 20:45:45 +0200

    Test : Test changes

--- tests/test.py
+++ tests/test.py
@@ -1,17 +1,28 @@
-from pyengine import Window, GameState
+from pyengine import Window, GameState, Entity
 from pyengine.Systems import *
+from pyengine.Components import PositionComponent, TextComponent
 from pyengine.Widgets import *
 
 
-game = Window(800, 600, (200, 145, 208), True)
+def click(pos, click):
+    text = entity.get_component(TextComponent)
+    text.get_font().set_size(text.get_font().get_size()+1)
+    bouton.get_label().set_text("TEST")
+
+
+game = Window(800, 600, (0, 0, 0), True)
 game.set_title("ShowHide TEST")
 state = GameState("BASE")
 game.add_state(state)
 
-entry = Entry([100, 100], 200, "images/sprite0.png")
-
+bouton = Button([0, 0], "CLICK", click)
+entity = Entity()
+entity.add_component(PositionComponent([100, 100]))
+entity.add_component(TextComponent("A"))
 
 system = state.get_system(UISystem)
-system.add_widget(entry)
+system.add_widget(bouton)
+system2 = state.get_system(EntitySystem)
+system2.add_entity(entity)
 
 game.run()
