44c9d9fc864a95c6dd07ebf3be027418f78b0c39
LavaPower <lavapower84@gmail.com>
2019-05-23 20:45:15 +0200

    Label : Use Font object

--- pyengine/Widgets/Label.py
+++ pyengine/Widgets/Label.py
@@ -1,5 +1,6 @@
 import pygame
 from pyengine.Widgets.Widget import Widget
+from pyengine.Utils import Font
 
 __all__ = ["Label"]
 
@@ -8,52 +9,45 @@ class Label(Widget):
     def __init__(self, position, text, color=None, font=None):
         super(Label, self).__init__(position)
         if font is None:
-            font = ["arial", 15, False, False]
+            font = Font()
         if color is None:
             color = (255, 255, 255)
 
-        if len(font) == 1:
-            font.append(15)
-        if len(font) == 2:
-            font.append(False)
-        if len(font) == 3:
-            font.append(False)
+        if not isinstance(font, Font):
+            raise TypeError("Font have not a Font type")
 
         self.color = color
-        self.font_infos = font
-        self.font = pygame.font.SysFont(font[0], font[1], font[2], font[3])
+        self.font = font
+        self.font.parent = self
         self.text = text
-        self.image = self.font.render(text, 1, color)
-        self.update_rect()
+        self.update_render()
 
     def set_color(self, color):
         self.color = color
-        self.image = self.font.render(self.text, 1, color)
-        self.update_rect()
+        self.update_render()
 
     def get_color(self):
         return self.color
 
     def set_font(self, font):
-        if len(font) == 1:
-            font.append(15)
-        if len(font) == 2:
-            font.append(False)
-        if len(font) == 3:
-            font.append(False)
-        self.font_infos = font
-        self.font = pygame.font.SysFont(font[0], font[1], font[2], font[3])
-        self.image = self.font.render(self.text, 1, self.color)
-        self.update_rect()
+        if not isinstance(font, Font):
+            raise TypeError("Font have not a Font type")
+
+        self.font = font
+        self.font.parent = self
+        self.update_render()
 
     def get_font(self):
-        return self.font_infos
+        return self.font
 
     def get_text(self):
         return self.text
 
     def set_text(self, text):
         self.text = text
-        self.image = self.font.render(text, 1, self.color)
+        self.update_render()
+
+    def update_render(self):
+        self.image = self.font.render().render(self.text, 1, self.color)
         self.update_rect()
 
