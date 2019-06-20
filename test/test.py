b0f0b487c107e79bb60f99696f251741b522c53e
LavaPower <lavapower84@gmail.com>
2019-06-20 19:49:42 +0200

    Test : Test Config System

--- test/test.py
+++ test/test.py
@@ -1,14 +1,18 @@
 from pyengine import Window
 from pyengine.Systems import UISystem
 from pyengine.Widgets import Label, Button
-from pyengine.Utils import Colors, Vec2, Lang
+from pyengine.Utils import Colors, Vec2, Lang, Config
+import os
 
 
 def setlang(obj, button):
     if lang.file == "langs/fr.lang":
         lang.file = "langs/en.lang"
+        conf.set("lang", "langs/en.lang")
     else:
         lang.file = "langs/fr.lang"
+        conf.set("lang", "langs/fr.lang")
+    conf.save()
     hello.text = lang.get_translate("label", "Hello !")
     b.label.text = lang.get_translate("button", "English")
 
@@ -17,7 +21,14 @@ fenetre = Window(500, 300, Colors.WHITE.value)
 
 uisystem = fenetre.world.get_system(UISystem)
 
-lang = Lang("langs/fr.lang")
+if os.path.exists("config.json"):
+    conf = Config("config.json")
+else:
+    conf = Config("config.json")
+    conf.create({
+        "lang": "langs/fr.lang"
+    })
+lang = Lang(conf.get("lang"))
 
 hello = Label(Vec2(10, 10), lang.get_translate("label", "Hello !"), Colors.BLACK.value)
 b = Button(Vec2(10, 100), lang.get_translate("button", "English"), setlang)
