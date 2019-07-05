ec192c934cd5434b13a5a8b2bc955d2ddd800e13
LavaPower <lavapower84@gmail.com>
2019-07-06 01:29:46 +0200

    Test ; Modification of Entry

--- test/game.py
+++ test/game.py
@@ -1,6 +1,6 @@
 from pyengine import Window
 from pyengine.Systems import UISystem
-from pyengine.Utils import Colors, Vec2
+from pyengine.Utils import Colors, Vec2, Font
 from pyengine.Widgets import Label, Button, Entry
 
 
@@ -12,7 +12,7 @@ class Game(Window):
 
         self.la = Label(Vec2(100, 100), "Texte", Colors.BLACK.value)
         self.b = Button(Vec2(100, 200), "Afficher", self.show_entry)
-        self.e = Entry(Vec2(100, 300))
+        self.e = Entry(Vec2(100, 300), color=Colors.GREEN.value, font=Font(size=25))
 
         self.uisys.add_widget(self.la)
         self.uisys.add_widget(self.b)
@@ -22,11 +22,6 @@ class Game(Window):
 
     def show_entry(self, btn, click):
         self.la.text = self.e.text
-        self.e.width = 500
-        if self.e.hasimage:
-            self.e.sprite = None
-        else:
-            self.e.sprite = "../tests/files/sprite0.png"
 
 
 Game()
