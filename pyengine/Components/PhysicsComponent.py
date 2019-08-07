0a9f171b893a47c9dadcd5a429ed759ab6e1a46a
LavaPower <lavapower84@gmail.com>
2019-08-07 17:51:48 +0200

    PhysicsComponent : Add properties

--- pyengine/Components/PhysicsComponent.py
+++ pyengine/Components/PhysicsComponent.py
@@ -14,11 +14,52 @@ class PhysicsComponent:
         self.origin_image = None
         self.body = None
         self.shape = None
-        self.affectbygravity = affectbygravity
-        self.friction = friction
-        self.elasticity = elasticity
-        self.mass = mass
+        self.__affectbygravity = affectbygravity
+        self.__friction = friction
+        self.__elasticity = elasticity
+        self.__mass = mass
         self.callback = callback
+
+    @property
+    def affectbygravity(self):
+        return self.__affectbygravity
+
+    @affectbygravity.setter
+    def affectbygravity(self, val):
+        if val:
+            self.body.body_type = self.body.DYNAMIC
+        else:
+            self.body.body_type = self.body.KINEMATIC
+        self.__affectbygravity = val
+
+    @property
+    def friction(self):
+        return self.__friction
+
+    @friction.setter
+    def friction(self, val):
+        self.shape.friction = val
+        self.__friction = val
+
+    @property
+    def elasticity(self):
+        return self.__elasticity
+
+    @elasticity.setter
+    def elasticity(self, val):
+        self.shape.elasticity = val
+        self.__elasticity = val
+
+    @property
+    def mass(self):
+        return self.__mass
+
+    @mass.setter
+    def mass(self, val):
+        moment = pymunk.moment_for_box(self.mass, (self.entity.rect.width, self.entity.rect.height))
+        self.body = pymunk.Body(self.mass, moment)
+        self.body.center_of_gravity = (self.entity.rect.width/2, self.entity.rect.height/2)
+        self.shape.body = self.body
 
     @property
     def entity(self):
@@ -29,10 +70,11 @@ class PhysicsComponent:
         self.__entity = entity
         self.origin_image = entity.image
         temp = entity.rect.width
-        vc = [(0, temp), (temp, temp), (temp, 0), (0, 0)]
+        temp2 = entity.rect.height
+        vc = [(0, temp2), (temp, temp2), (temp, 0), (0, 0)]
         moment = pymunk.moment_for_box(self.mass, (entity.rect.width, entity.rect.height))
         self.body = pymunk.Body(self.mass, moment)
-        self.body.center_of_gravity = (temp/2, temp/2)
+        self.body.center_of_gravity = (temp/2, temp2/2)
         if not self.affectbygravity:
             self.body.body_type = self.body.KINEMATIC
         self.shape = pymunk.Poly(self.body, vc)
@@ -53,7 +95,6 @@ class PhysicsComponent:
         self.entity.image = pygame.transform.rotate(self.origin_image, math.degrees(self.body.angle))
 
         if self.entity.has_component(PositionComponent):
-            print(self.flipy(self.body.position), self.entity.get_component(PositionComponent).position)
             self.entity.get_component(PositionComponent).position = Vec2(self.flipy(self.body.position))
 
     def update_pos(self, pos):
