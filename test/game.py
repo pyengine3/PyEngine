51fedbd798cd09ec68865ab67a5a25374e2ec9f4
LavaPower <lavapower84@gmail.com>
2019-08-09 00:40:08 +0200

    Game : Make a better demo

--- test/game.py
+++ test/game.py
@@ -1,8 +1,9 @@
-from pyengine import Window, ControlType, MouseButton
+from pyengine import Window, WindowCallbacks, MouseButton
 from pyengine.Systems import EntitySystem
 from pyengine.Entities import Entity
-from pyengine.Components import PositionComponent, SpriteComponent, ControlComponent, PhysicsComponent
+from pyengine.Components import PositionComponent, SpriteComponent, PhysicsComponent
 from pyengine.Utils import Vec2
+import math
 
 
 class BasicEntity(Entity):
@@ -17,23 +18,35 @@ class Game(Window):
     def __init__(self):
         super(Game, self).__init__(1200, 750, debug=True)
 
+        self.set_callback(WindowCallbacks.RUNWINDOW, self.launch)
+
         self.world.mousepress = self.mousepress
         self.esys = self.world.get_system(EntitySystem)
 
-        j = BasicEntity(Vec2(300, 600))
-        j.get_component(SpriteComponent).size = Vec2(600, 20)
-        j.add_component(ControlComponent(ControlType.FOURDIRECTION))
-        j.add_component(PhysicsComponent(False, callback=self.collision))
+        bas = BasicEntity(Vec2(600, 700))
+        bas.get_component(SpriteComponent).size = Vec2(800, 20)
+        bas.add_component(PhysicsComponent(False, elasticity=1))
+
+        self.gauche = BasicEntity(Vec2(50, 500))
+        self.gauche.get_component(SpriteComponent).size = Vec2(500, 20)
+        self.gauche.add_component(PhysicsComponent(False, elasticity=1))
+
+        self.droit = BasicEntity(Vec2(1150, 500))
+        self.droit.get_component(SpriteComponent).size = Vec2(500, 20)
+        self.droit.add_component(PhysicsComponent(False, elasticity=1))
 
         obj = BasicEntity(Vec2(100, 100))
-        obj.add_component(PhysicsComponent(elasticity=1.5))
+        obj.add_component(PhysicsComponent(elasticity=1))
 
-        self.esys.add_entity(j)
+        self.esys.add_entity(bas)
+        self.esys.add_entity(self.gauche)
+        self.esys.add_entity(self.droit)
         self.esys.add_entity(obj)
         self.run()
 
-    def collision(self, entity, others, space, data):
-        pass
+    def launch(self):
+        self.gauche.get_component(PhysicsComponent).body.angle = math.radians(-60)
+        self.droit.get_component(PhysicsComponent).body.angle = math.radians(60)
 
     def mousepress(self, evt):
         self.world.systems["Entity"].mousepress(evt)
@@ -41,7 +54,8 @@ class Game(Window):
 
         if evt.button == MouseButton.LEFTCLICK.value:
             obj = BasicEntity(Vec2(evt.pos))
-            obj.add_component(PhysicsComponent(elasticity=1.5))
+            obj.add_component(PhysicsComponent(elasticity=1))
             self.esys.add_entity(obj)
+
 
 Game()
