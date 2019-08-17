4f7ce5e4c8c2840b0b22a8ef2fce21aaac84a62d
LavaPower <lavapower84@gmail.com>
2019-08-17 22:22:51 +0200

    PhysicsComponent : Add check_grounding function

--- pyengine/Components/PhysicsComponent.py
+++ pyengine/Components/PhysicsComponent.py
@@ -111,4 +111,27 @@ class PhysicsComponent:
     def update_rot(self, rot):
         self.body.angle = math.radians(rot)
 
+    def check_grounding(self):
+        """ See if the player is on the ground. Used to see if we can jump. """
+        grounding = {
+            'normal': pymunk.Vec2d.zero(),
+            'penetration': pymunk.Vec2d.zero(),
+            'impulse': pymunk.Vec2d.zero(),
+            'position': pymunk.Vec2d.zero(),
+            'body': None
+        }
+
+        def f(arbiter):
+            n = -arbiter.contact_point_set.normal
+            if n.y > grounding['normal'].y:
+                grounding['normal'] = n
+                grounding['penetration'] = -arbiter.contact_point_set.points[0].distance
+                grounding['body'] = arbiter.shapes[1].body
+                grounding['impulse'] = arbiter.total_impulse
+                grounding['position'] = arbiter.contact_point_set.points[0].point_b
+
+        self.body.each_arbiter(f)
+
+        return grounding
+
 
