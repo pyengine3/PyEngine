72a9c976378f4b7db92903ff82c537d61bf81b18
LavaPower <lavapower84@gmail.com>
2019-08-27 12:44:06 +0200

    Update game.py

--- test/game.py
+++ test/game.py
@@ -1,87 +1,78 @@
-import math
-
-from pyengine import Window, MouseButton
-from pyengine.Components import PositionComponent, SpriteComponent, PhysicsComponent
+from pyengine import Window, const, Controls, ControlType, WindowCallbacks
 from pyengine.Entities import Entity
-from pyengine.Systems import EntitySystem, UISystem
-from pyengine.Widgets import Image, Entry, Selector
-from pyengine.Utils import Vec2, Colors
+from pyengine.Components import PositionComponent, SpriteComponent, PhysicsComponent, MoveComponent, ControlComponent
+from pyengine.Systems import EntitySystem
+from pyengine.Utils import Colors, Vec2
+from random import randint
 
 
-class BasicEntity(Entity):
-    def __init__(self, pos):
-        super(BasicEntity, self).__init__()
-
-        self.add_component(PositionComponent(pos))
-        self.add_component(SpriteComponent("images/sprite0.png", 2))
-
-
-class Game(Window):
+class Jeu:
     def __init__(self):
-        super(Game, self).__init__(1300, 650, debug=True)
+        self.window = Window(800, 400, Colors.WHITE.value)
+        self.window.title = "Pong"
 
-        self.world.mousepress = self.mousepress
-        self.esys = self.world.get_system(EntitySystem)
-        self.uisys = self.world.get_system(UISystem)
+        self.window.set_callback(WindowCallbacks.OUTOFWINDOW, self.outofwindow)
 
-        bas = BasicEntity(Vec2(650, 600))
-        bas.get_component(SpriteComponent).size = Vec2(800, 20)
-        bas.add_component(PhysicsComponent(False, elasticity=.9))
+        self.j1 = Entity()
+        self.j1.add_component(PositionComponent(Vec2(10, 175)))
+        spritej1 = self.j1.add_component(SpriteComponent("images/sprite0.png"))
+        spritej1.size = Vec2(20, 50)
+        controlj1 = self.j1.add_component(ControlComponent(ControlType.UPDOWN, 3))
+        controlj1.set_control(Controls.UPJUMP, const.K_w)
+        controlj1.set_control(Controls.DOWN, const.K_s)
+        self.j1.add_component(PhysicsComponent(False))
 
-        gauche = BasicEntity(Vec2(100, 400))
-        gauche.get_component(SpriteComponent).size = Vec2(500, 20)
-        gauche.get_component(SpriteComponent).rotation = -60
-        gauche.add_component(PhysicsComponent(False, elasticity=.9))
+        self.j2 = Entity()
+        self.j2.add_component(PositionComponent(Vec2(770, 175)))
+        spritej2 = self.j2.add_component(SpriteComponent("images/sprite0.png"))
+        spritej2.size = Vec2(20, 50)
+        controlj2 = self.j2.add_component(ControlComponent(ControlType.UPDOWN, 3))
+        controlj2.set_control(Controls.UPJUMP, const.K_UP)
+        controlj2.set_control(Controls.DOWN, const.K_DOWN)
+        self.j2.add_component(PhysicsComponent(False))
 
-        droit = BasicEntity(Vec2(1200, 400))
-        droit.get_component(SpriteComponent).size = Vec2(500, 20)
-        droit.get_component(SpriteComponent).rotation = 60
-        droit.add_component(PhysicsComponent(False, elasticity=.9))
+        self.ball = Entity()
+        self.ball.add_component(PositionComponent(Vec2(390, 190)))
+        spriteballe = self.ball.add_component(SpriteComponent("images/sprite0.png"))
+        spriteballe.size = Vec2(20, 20)
+        physball = self.ball.add_component(PhysicsComponent(False))
+        physball.callback = self.collision
+        self.ball.add_component(MoveComponent(Vec2(randint(50, 100), randint(50, 100))))
 
-        spinner = BasicEntity(Vec2(650, 450))
-        spinner.get_component(SpriteComponent).size = Vec2(300, 20)
-        phys = spinner.add_component(PhysicsComponent(False, elasticity=.9))
-        phys.body.angular_velocity = math.radians(120)
+        entitysystem = self.window.world.get_system(EntitySystem)
+        entitysystem.add_entity(self.j1)
+        entitysystem.add_entity(self.j2)
+        entitysystem.add_entity(self.ball)
 
-        spinner2 = BasicEntity(Vec2(1050, 300))
-        spinner2.get_component(SpriteComponent).size = Vec2(300, 20)
-        phys2 = spinner2.add_component(PhysicsComponent(False, elasticity=.9))
-        phys2.body.angular_velocity = math.radians(120)
+        self.window.run()
 
-        spinner3 = BasicEntity(Vec2(250, 300))
-        spinner3.get_component(SpriteComponent).size = Vec2(300, 20)
-        phys3 = spinner3.add_component(PhysicsComponent(False, elasticity=.9))
-        phys3.body.angular_velocity = math.radians(120)
+    def collision(self, entity, others, space, data):
+        move = entity.get_component(MoveComponent)
+        move.direction = Vec2(-move.direction.x, move.direction.y)
 
-        obj = BasicEntity(Vec2(150, 100))
-        obj.add_component(PhysicsComponent(elasticity=.9, can_rot=False))
+    def outofwindow(self, obj, pos):
+        if obj == self.j1:
+            position = self.j1.get_component(PositionComponent)
+            if pos.y <= obj.rect.height / 2:
+                position.position = Vec2(10, obj.rect.height / 2)
+            else:
+                position.position = Vec2(10, 400 - obj.rect.height / 2)
 
-        self.esys.add_entity(bas)
-        self.esys.add_entity(gauche)
-        self.esys.add_entity(droit)
-        self.esys.add_entity(spinner)
-        self.esys.add_entity(spinner2)
-        self.esys.add_entity(spinner3)
-        self.esys.add_entity(obj)
+        elif obj == self.j2:
+            position = self.j2.get_component(PositionComponent)
+            if pos.y <= obj.rect.height / 2:
+                position.position = Vec2(770, obj.rect.height / 2)
+            else:
+                position.position = Vec2(770, 400 - obj.rect.height / 2)
 
-        back = Image(Vec2(0, 0), "images/sprite1.png")
-        entry = Entry(Vec2(100, 100))
-        select = Selector(Vec2(200, 200), ["Mamadou", "Charo"])
-        select.label.color = Colors.WHITE.value
-        self.uisys.add_widget(back)
-        self.uisys.add_widget(entry)
-        self.uisys.add_widget(select)
-
-        self.run()
-
-    def mousepress(self, evt):
-        self.world.systems["Entity"].mousepress(evt)
-        self.world.systems["UI"].mousepress(evt)
-
-        if evt.button == MouseButton.LEFTCLICK.value:
-            obj = BasicEntity(Vec2(evt.pos))
-            obj.add_component(PhysicsComponent(elasticity=.9))
-            self.esys.add_entity(obj)
+        else:
+            if pos.x < 10 or pos.x > 790:
+                position = self.ball.get_component(PositionComponent)
+                position.position = Vec2(390, 190)
+                self.ball.get_component(MoveComponent).direction = Vec2(randint(50, 100), randint(50, 100))
+            else:
+                move = self.ball.get_component(MoveComponent)
+                move.direction = Vec2(move.direction.x, -move.direction.y)
 
 
-Game()
+Jeu()
